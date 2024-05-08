178e393892c2a86e12c5a6d2e0c781d821651be2
Kamil <kdnuriev@gmail.com>
2024-05-08 23:13:55 +0400

    first task

--- organizations/party/Election.java
+++ organizations/party/Election.java
@@ -8,5 +8,35 @@ public class Election {
 
     private boolean fair;
 
+    public Election(int votersNumber, boolean fair, String description) {
+        this.votersNumber = votersNumber;
+        this.fair = fair;
+        this.description = description;
+    }
+
     private String description;
+
+    public int getVotersNumber() {
+        return votersNumber;
+    }
+
+    public void setVotersNumber(int votersNumber) {
+        this.votersNumber = votersNumber;
+    }
+
+    public boolean isFair() {
+        return fair;
+    }
+
+    public void setFair(boolean fair) {
+        this.fair = fair;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
--- organizations/party/Party.java
+++ organizations/party/Party.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Party extends Organization implements Contactable {
+    private final String name;
     String mission;
 
     String email, address, phoneNumber;
@@ -15,24 +16,30 @@ public class Party extends Organization implements Contactable {
 
     public Party(String name, String mission) {
         // TODO finish this method (0.25 points)
-
+        this.name = name;
+        this.mission = mission;
     }
 
     public void addElection(Election e) {
         // TODO finish this method (0.25 points)
+        elections.add(e);
     }
 
     public boolean removeElection(Election e) {
         boolean removed = false;
         // TODO finish this method (0.25 points)
-
+        removed = elections.remove(e);
         return removed;
     }
 
     public List<Election> getFairElections() {
         List<Election> fairElections = new ArrayList<>();
         // TODO finish this method (0.25 points)
-
+        for(int i = 0; i < elections.size(); i++) {
+            if (elections.get(i).isFair()) {
+                fairElections.add(elections.get(i));
+            }
+        }
         return fairElections;
     }
 
